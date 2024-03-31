c50ca0e4582ed8c6fd14e8746985d436c791f6d8
Junior Oliveira Leandro <contato_jr@outlook.com>
2024-03-30 23:56:48 -0300

    bot.js add

---
+++ bot.js
@@ -0,0 +1,23 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client();
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+
+client.on('message_create', message => {
+	if (message.body === '!ping') {
+		// send back "pong" to the chat the message was sent in
+		client.sendMessage(message.from, 'pong');
+	}
+});
+
+
+client.initialize();
