edc12e246853d840acc66de67f7e4da5819d75b6
Junior Oliveira Leandro <contato_jr@outlook.com>
2024-04-01 10:16:11 -0300

    finalização do projeto demo

--- bot.js
+++ bot.js
@@ -1,49 +1,77 @@
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-
 const client = new Client();
 
+// Mensagem informando que o servidor está pronto
 client.on('ready', () => {
 	console.log('Client is ready!');
 });
 
+// Gera o QR-Code para conectar com o WhatsApp
 client.on('qr', qr => {
 	qrcode.generate(qr, { small: true });
 });
 
 
 client.on('message_create', message => {
-	if (message.body.toLocaleLowerCase() === 'ola') {
-		// send back "pong" to the chat the message was sent in
-		client.sendMessage(message.from, 'Olá, bem vindo(a). Não estou disponivel no momento.');
-		client.sendMessage(message.from, 'Caso queira conversar comigo, por favor escolha uma das opções abaixo para agilizar seu atendimento. Digite apenas o número da opção desejada:');
-		client.sendMessage(message.from, '1. Falar sobre a barbearia');
-		client.sendMessage(message.from, '2. Falar sobre Desenvolvimento Front-End');
-		client.sendMessage(message.from, '3. Falar sobre a Missão Enchei-vos');
-		client.sendMessage(message.from, '4. Falar sobre a Comunidade Santo Expedito');
-		client.sendMessage(message.from, '5. Falar sobre outros assuntos');
+	// Quando a conversa recebe a palavra "atendimento" o bot retorna as respostas abaixo
+	if (message.body.toLocaleLowerCase() === 'atendimento') {
+		// Mensagem retornada com as opções que podem ser escolhidas
+		client.sendMessage(message.from, 'Olá, bem vindo(a)! Para agilizar seu atendimento, utilize uma das opções abaixo. Digite apenas o número da opção desejada:');
+		client.sendMessage(message.from, '1. Falar sobre o assunto 1');
+		client.sendMessage(message.from, '2. Falar sobre o assunto 2');
+		client.sendMessage(message.from, '3. Falar sobre o assunto 3');
+		client.sendMessage(message.from, '4. Falar sobre o assunto 4');
+		client.sendMessage(message.from, '5. Falar sobre o assunto 5');
 	}
+	
+	// Quando a conversa recebe "1" o bot retorna a(s) resposta(s) abaixo
 	if (message.body.toLocaleLowerCase() === '1') {
-		client.sendMessage(message.from, 'Gostaria de agendar um horário? Basta acessar o endereço https://irulu54.gendo.app/ e reservar o melhor dia e horario para você. Caso o assunto seja outro, por favor, escreva abaixo e aguarde, te retorno assim que possivel. Obrigado');
-
+		client.sendMessage(message.from, 'Mensagem a respeito do assunto 1');
 	}
+
+	// Quando a conversa recebe "2" o bot retorna a(s) resposta(s) abaixo
 	if (message.body.toLocaleLowerCase() === '2') {
-		client.sendMessage(message.from, 'Que legal! Vamos conversar sobre o que? Pode me dar uma breve descrição do que se trata? Deste modo, assim que eu acessar o WhatsApp posso te retornar ja sabendo do que se trata. Já deu uma olhdinha no meu portifólio? http://junior.dev.br Obrigado desde ja por seu contato');
-
+		client.sendMessage(message.from, 'Mensagem a respeito do assunto 2');
+		client.sendMessage(message.from, '2ª Linha da mensagem a respeito do assunto 2');
 	}
+
+	// Quando a conversa recebe "3" o bot retorna a(s) resposta(s) abaixo
 	if (message.body.toLocaleLowerCase() === '3') {
-		client.sendMessage(message.from, 'A Paz de Jesus e o amor de Maria seja a causa da nossa alegria todos os dias! Como posso te ajudar irmão?');
-
+		client.sendMessage(message.from, 'Mensagem a respeito do assunto 3');
+		// Podendo inclusive passar mais opções
+		client.sendMessage(message.from, 'Para mais uma opção digite 6');
+		client.sendMessage(message.from, 'Para mais uma opção digite 7');
 	}
+
+	// Quando a conversa recebe "4" o bot retorna a(s) resposta(s) abaixo
 	if (message.body.toLocaleLowerCase() === '4') {
-		client.sendMessage(message.from, 'Que a Paz de Jesus e o amor de Maria esteja sempre em nossos corações! Como posso te ajudar irmão?');
-
+		client.sendMessage(message.from, 'Mensagem a respeito do assunto 4');
+		// Pode-se pedir uma palavra também
+		client.sendMessage(message.from, 'Caso queira falar sobre maçãs digite "Maçã".');
 	}
+
+	// Quando a conversa recebe "5" o bot retorna a(s) resposta(s) abaixo
 	if (message.body.toLocaleLowerCase() === '5') {
-		client.sendMessage(message.from, 'Opa! Como posso te ajudar irmão?');
-
+		client.sendMessage(message.from, 'Mensagem a respeito do assunto 5');
+		client.sendMessage(message.from, '2ª Linha da mensagem a respeito do assunto 5');
+		client.sendMessage(message.from, '3ª Linha da mensagem a respeito do assunto 5');
 	}
 
+	// Sub-opção do item 3	
+	if (message.body.toLocaleLowerCase() === '6') {
+		client.sendMessage(message.from, 'Mensagem a respeito da opção 6 do assunto 3');
+	}
+	// Sub-opção do item 3	
+	if (message.body.toLocaleLowerCase() === '7') {
+		client.sendMessage(message.from, 'Mensagem a respeito da opção 7 do assunto 3');
+	}
+	
+	// Sub-opção do item 4 pedindo uma palavra ao invéz de um numero
+	if (message.body.toLocaleLowerCase() === 'maçã') {
+		client.sendMessage(message.from, 'Maçãs são deliciosas!');
+	}
+	
 });
 
 
